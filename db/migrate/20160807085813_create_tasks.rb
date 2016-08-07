62ade2552bcccbc112580a05d805362a4ee55798
Shailendra Singh <shail@Shailendras-MacBook-Pro.local>
2016-08-07 15:26:24 +0530

    User and Task Migrations Added

--- db/migrate/20160807085813_create_tasks.rb
+++ db/migrate/20160807085813_create_tasks.rb
@@ -1,7 +1,12 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-
+    	t.integer :user_id
+    	t.string :title
+    	t.string :desctiption
+    	t.string :deadline
+    	t.integer :priority
+    	t.string :category
       t.timestamps null: false
     end
   end
